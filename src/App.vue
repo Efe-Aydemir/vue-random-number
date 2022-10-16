a7751fc4c434e1b760f423dd1ed8c41d6fc0b11e
Efe Aydemir <delidoluefe32@gmail.com>
2022-10-16 09:58:19 +0300

    Created Random Card

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="root-div">
+    <h1 class="text-3xl text-yellow-300 font-bold text-center pt-5">Random Game</h1>
+    <MRRandomCard />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import MRRandomCard from './components/MRRandomCard.vue';
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.root-div {
+  width: 100%;
+  height: 100vh;
+  background-image: linear-gradient(to right, rgb(0, 216, 180), rgb(0, 119, 255));
 }
 </style>
---
+++ src/components/MRRandomCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="root" class="flex justify-center items-center pt-10">
+        <div class="bg-white px-40 py-5 rounded-xl shadow-lg shadow-black flex flex-col">
+            <h1 class="text-xl ">Number</h1>
+            <pre>{{number}}</pre>
+            <button class="w-32 h-12 bg-black text-white" @click="basicnumber">click</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+let number = ref(0)
+
+function basicnumber() {
+    number.value = Math.floor(Math.random() * 11);
+}
+</script>
