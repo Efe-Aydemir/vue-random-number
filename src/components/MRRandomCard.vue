96cd19b084d162c477e21b2f698e2889d2783d5b
Efe Aydemir <delidoluefe32@gmail.com>
2022-11-08 20:02:58 +0300

    Level Designed

--- src/components/MRRandomCard.vue
+++ src/components/MRRandomCard.vue
@@ -1,19 +1,21 @@
 <template>
     <div id="root" class="flex justify-center items-center pt-10">
         <div class="bg-white px-40 py-5 rounded-xl shadow-lg shadow-black flex flex-col">
-            <h1 class="text-xl ">Number</h1>
-            <pre>{{number}}</pre>
-            <button class="w-32 h-12 bg-black text-white" @click="basicnumber">click</button>
+            <div class="flex flex-col gap-5" v-if="islook">
+                <button class="w-32 h-12 transition-all duration-300 bg-green-400 text-black rounded-xl hover:shadow-xl hover:shadow-black" @click="easy">easy level</button>
+            <button class="w-32 h-12 bg-yellow-500 transition-all duration-300 text-black rounded-xl hover:shadow-xl hover:shadow-black" @click="normal">normal level</button>
+            <button class="w-32 h-12 bg-red-700 text-white rounded-xl transition-all duration-300 hover:shadow-xl hover:shadow-black" @click="hard">hard level</button>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import {ref} from 'vue';
 
-let number = ref(0)
+let islook = ref(true)
 
-function basicnumber() {
-    number.value = Math.floor(Math.random() * 11);
+function easy() {
+    islook.value = false
 }
 </script>
