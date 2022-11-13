e11a47e27a7e6150fd824674ad7acbbd57963c0f
Efe Aydemir <delidoluefe32@gmail.com>
2022-11-13 16:32:03 +0300

    update: game sounds

--- src/components/MRRandomCard.vue
+++ src/components/MRRandomCard.vue
@@ -13,6 +13,10 @@
             <button class="w-44 h-12 bg-yellow-500 transition-all duration-300 text-black rounded-xl hover:shadow-xl hover:shadow-yellow-800" @click="normal">normal level (0 to 50)</button>
             <button class="w-44 h-12 bg-red-700 text-white rounded-xl transition-all duration-300 hover:shadow-xl hover:shadow-red-900" @click="hard">hard level (0 to 100)</button>
             </div>
+            <div class="music flex flex-col items-center p-5">
+                <img v-if="musicview" @click="musicopen" src="@/assets/img/close.png" width="50" alt="Close" class="cursor-pointer"/>
+                <img v-if="musicview===false" @click="musicoff" src="@/assets/img/open.png" width="50" alt="Open" class="cursor-pointer">
+            </div>
         </div>
         <div class="inset-0 transition-all duration-1000 bg-white absolute flex justify-center items-center" :class="corr">
             <h1 class="text-5xl">{{restext}}</h1>
@@ -22,7 +26,10 @@
 
 <script setup>
 import {ref} from 'vue';
-
+let music = require('@/assets/sounds/music.mp3')
+let falsesound = require('@/assets/sounds/false.mp3')
+let corsound = require('@/assets/sounds/correct.mp3')
+let endsound = require('@/assets/sounds/end.mp3')
 let islook = ref(true)
 let islvllook = ref(false)
 let number = ref()
@@ -33,12 +40,45 @@ let corr = ref('opacity-0 invisible')
 let invalue = ref()
 let restext = ref('')
 let heart = ref(5)
+let musicview = ref(true)
+
+
+let audio = new Audio(music)
+
+
+function falsee() {
+    let falseso = new Audio(falsesound)
+    falseso.play()
+}
+
+function coreect() {
+    let corree = new Audio(corsound)
+    corree.play()
+}
+
+function end() {
+    let endd = new Audio(endsound)
+    endd.play()
+}
+
+function musicopen() {
+    musicview.value = false
+    audio.play()
+}
+
+
+function musicoff() {
+    musicview.value = true
+    audio.pause()
+}
+
 
 function easy() {
     islook.value = false
     islvllook.value = true
     eclick.value = true
     number.value = Math.floor(Math.random() * 11)
+    console.log(number.value)
 }
 
 function normal() {
@@ -70,6 +110,7 @@ function apply() {
     
     if(invalue.value === number.value) {
         restext.value = 'Correct'
+        coreect()
         corr.value = 'opacity-100 visible'
         invalue.value = ''
         if (heart.value < 5) {
@@ -79,6 +120,8 @@ function apply() {
         }
         if (eclick.value) {
         number.value = Math.floor(Math.random() * 11)
+    console.log(number.value)
+
     } else if (nclick.value) {
         number.value = Math.floor(Math.random() * 51)
     } else if (hclick.value) {
@@ -86,13 +129,20 @@ function apply() {
     }
     } else {
         restext.value = 'False'
+        if (heart.value > 1) {
+            falsee()
+        } else {
+            null
+        }
         corr.value = 'opacity-100 visible'
         invalue.value = ''
         heart.value -= 1
+        
     }
 
     if (heart.value < 1) {
         restext.value = 'Your life is over, you lost the game'
+        end()
         islook.value = true
     islvllook.value = false
     number.value = 
@@ -106,7 +156,7 @@ function apply() {
 
     setTimeout(() => {
         corr.value = 'opacity-0 invisible'
-    }, 2000);
+    }, 700);
 }
 
 </script>
